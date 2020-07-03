d57a8cf6920ff7a0b19b518d0d70b1d262b6e4ba
atrujill <58313460+atrujill@users.noreply.github.com>
2020-07-03 15:41:28 -0300

    created plot4.R

--- plot4.R
+++ plot4.R
@@ -2,7 +2,7 @@
 data <- read.table("household_power_consumption.txt", header= TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 summary(data)
 
-
+#
 subsetdata <- data[data$Date %in% c("1/2/2007","2/2/2007"),]
 
 globalActivePower <- as.numeric(subsetdata$Global_active_power)
