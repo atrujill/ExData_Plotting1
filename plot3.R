6d734bf84841598869c2f823d3cf25520f2d1a5c
atrujill <58313460+atrujill@users.noreply.github.com>
2020-07-03 15:41:13 -0300

    created plot3.R

--- plot3.R
+++ plot3.R
@@ -1,7 +1,7 @@
 
 data <- read.table("household_power_consumption.txt", header= TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 summary(data)
-
+#
 
 subsetdata <- data[data$Date %in% c("1/2/2007","2/2/2007"),]
 
